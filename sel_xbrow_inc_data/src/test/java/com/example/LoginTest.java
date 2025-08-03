06591e6b0294db40cfcd154ce0b18531c8415ece
FrankYSerrano <FrankYSerrano@GMail.Com>
2025-08-02 23:06:21 -0400

    Test refactorizado para inicializar el webdriver por separado e incluir el result URL en el DataProvider

--- sel_xbrow_inc_data/src/test/java/com/example/LoginTest.java
+++ sel_xbrow_inc_data/src/test/java/com/example/LoginTest.java
@@ -14,33 +14,36 @@ public class LoginTest {
     @DataProvider(name = "loginData")
     public Object[][] loginData() {
         return new Object[][] {
-            {"firefox", "standard_user", "secret_sauce"},
-            {"firefox", "locked_out_user", "secret_sauce"},
-            {"firefox", "problem_user", "secret_sauce"},
-            {"firefox", "performance_glitch_user", "secret_sauce"},
-            {"firefox", "error_user", "secret_sauce"},
-            {"firefox", "visual_user", "secret_sauce"},
-            {"chrome", "standard_user", "secret_sauce"},
-            {"chrome", "locked_out_user", "secret_sauce"},
-            {"chrome", "problem_user", "secret_sauce"},
-            {"chrome", "performance_glitch_user", "secret_sauce"},
-            {"chrome", "error_user", "secret_sauce"},
-            {"chrome", "visual_user", "secret_sauce"}
+            {"firefox", "standard_user", "secret_sauce", "https://www.saucedemo.com/inventory.html"},
+            {"firefox", "locked_out_user", "secret_sauce", "https://www.saucedemo.com/"},
+            {"firefox", "problem_user", "secret_sauce", "https://www.saucedemo.com/inventory.html"},
+            {"firefox", "performance_glitch_user", "secret_sauce", "https://www.saucedemo.com/inventory.html"},
+            {"firefox", "error_user", "secret_sauce", "https://www.saucedemo.com/inventory.html"},
+            {"firefox", "visual_user", "secret_sauce", "https://www.saucedemo.com/inventory.html"},
+            {"chrome", "standard_user", "secret_sauce", "https://www.saucedemo.com/inventory.html"},
+            {"chrome", "locked_out_user", "secret_sauce", "https://www.saucedemo.com/"},
+            {"chrome", "problem_user", "secret_sauce", "https://www.saucedemo.com/inventory.html"},
+            {"chrome", "performance_glitch_user", "secret_sauce", "https://www.saucedemo.com/inventory.html"},
+            {"chrome", "error_user", "secret_sauce", "https://www.saucedemo.com/inventory.html"},
+            {"chrome", "visual_user", "secret_sauce", "https://www.saucedemo.com/inventory.html"}
         };
     }
 
-    @Test(dataProvider = "loginData")
-    public void testLogin(String browser, String username, String password) {
-        WebDriver driver = null;
+    private WebDriver createWebDriver(String browser) {
         if ("firefox".equalsIgnoreCase(browser)) {
             WebDriverManager.firefoxdriver().setup();
-            driver = new FirefoxDriver();
+            return new FirefoxDriver();
         } else if ("chrome".equalsIgnoreCase(browser)) {
             WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+            return new ChromeDriver();
         } else {
             throw new IllegalArgumentException("Unsupported browser: " + browser);
         }
+    }
+
+    @Test(dataProvider = "loginData")
+    public void testLogin(String browser, String username, String password, String expectedUrl) {
+        WebDriver driver = createWebDriver(browser);
 
         try {
             driver.get("https://www.saucedemo.com/");
@@ -49,20 +52,8 @@ public class LoginTest {
             driver.findElement(By.id("login-button")).click();
 
             String currentUrl = driver.getCurrentUrl();
-
-            if ("standard_user".equals(username) 
-                || "problem_user".equals(username)
-                || "performance_glitch_user".equals(username) 
-                || "error_user".equals(username) 
-                || "visual_user".equals(username)) {
-                Assert.assertEquals(currentUrl, 
-                    "https://www.saucedemo.com/inventory.html", 
-                    "Login should succeed for: " + username + " on " + browser);
-            } else {
-                Assert.assertEquals(currentUrl, 
-                    "https://www.saucedemo.com/", 
-                    "Login should fail for: " + username + " on " + browser);
-            }
+            Assert.assertEquals(currentUrl, expectedUrl, 
+                "Login result for: " + username + " on " + browser);
         } finally {
             if (driver != null) {
                 driver.quit();
